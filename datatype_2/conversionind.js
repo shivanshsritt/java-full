caa5ef22dec5cf0997ee2c4a7d7f9ebf4af0cc99
shivansh srivastava <150130781+shivanshsritt@users.noreply.github.com>
2024-03-04 19:43:41 +0530

    basic conversion  concept

---
+++ datatype_2/conversionind.js
@@ -0,0 +1,31 @@
+
+// 
+let a = "45d" ;
+console.log(typeof a);
+
+// conversion 
+
+let b =Number(a);
+console.log(b);
+
+
+// "33" <=   33   string convert into in number
+//  "33bw" <=  numberandstring convert in nun 
+
+
+let t = 0 ;
+
+let bool = Boolean (t);
+
+console.log(bool);
+
+
+
+
+
+let d = "shivasn";
+console.log(typeof d);
+let f = Number (d);
+console.log(f);
+
+
