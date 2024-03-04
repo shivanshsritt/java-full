7d72492af988883238e4dad3d3768a95a123c642
shivansh srivastava <150130781+shivanshsritt@users.noreply.github.com>
2024-03-04 13:24:33 +0530

    manipulator

---
+++ datatype_2/manipulation.js
@@ -0,0 +1,31 @@
+// reassigned
+  var  x = 4;
+  var  x =5;
+  x = x +6;
+  console.log(x );
+
+
+  //  block scope
+
+  {
+    let x =5 ;
+    console.log(x);
+  }
+
+
+  // global scope 
+
+ {
+  var x = "sonu";
+ } 
+  var x = "shivansh";
+  console.log(x)
+
+
+  // hoisting 
+
+  carname = "BMW";
+  var   carname;
+  console.log(carname);
+ 
+
