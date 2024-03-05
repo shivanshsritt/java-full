753b5e164b53e295e1f53a8b7aa6149c7c1398bb
shivansh srivastava <150130781+shivanshsritt@users.noreply.github.com>
2024-03-05 19:56:13 +0530

    just pra

---
+++ opertorpractice_4/pra1.js
@@ -0,0 +1,40 @@
+//  arithmitic operation 
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(22);
+console.log(2/2);
+console.log(22);
+
+
+
+let star = "aks"
+let star1 = " university"
+let star2 = star + star1;
+console.log(star2); 
+
+console.log(2+2 ,(3*3) , 23-45);
+
+
+console.log(+true);
+console.log(+"")
+
+
+let  num1, num2, num3 ;
+num1 =45;
+num2= 78;
+num3 = 72;
+console.log(num1);
+
+
+
+
+
+
+
+
+
+
