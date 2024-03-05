739f47cea09be4fc9026cfc6bd5b767238cf9e08
shivansh srivastava <150130781+shivanshsritt@users.noreply.github.com>
2024-03-05 19:55:08 +0530

    prac just

---
+++ opertorpractice_4/typeofopertor.js
@@ -0,0 +1,46 @@
+// additional  assignment  opertor 
+let shiv  =  200;
+shiv +=  50;
+console.log()
+
+
+
+
+// 
+let x =5;
+const y =  x++;
+
+console.log(x);
+
+  
+// 
+
+// let b = 50;
+// var a= b--;
+// console.log(b);
+
+
+
+
+// comparison opertor 
+
+let a = 2
+let b = 3;
+console.log(a>b);
+
+
+let c = 6;
+let d = 7;
+
+console.log(d>c);
+
+
+
+
+let e = 10 ;
+let f = 99;
+console.log(e<f);
+
+
+console.log (16%3);
+
