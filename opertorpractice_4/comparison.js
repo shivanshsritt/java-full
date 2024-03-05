7ca4e5ea768d05dc72084038ff5b45e61234f5ee
shivansh srivastava <150130781+shivanshsritt@users.noreply.github.com>
2024-03-05 19:58:33 +0530

    move

---
+++ opertorpractice_4/comparison.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 10;
+console.log(a==b);
+
+
+let c = 21;
+let d = 22;
+console.log(c!=d)
+
+
+
+
+let e = 22;
+let f = 44;
+console.log(e<= f);
+
+
+
+
+let i = 22;
+let h = 44;
+console.log(i>= h);
+
+console.log(null>);
